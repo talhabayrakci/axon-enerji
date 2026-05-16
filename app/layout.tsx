bf14266c7dad8d03614fcb03b8acda3b2e0fe21a
talhabayrakci <talhabayrakci@gmail.com>
2026-05-16 10:21:34 +0300

    site update

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,28 +50,7 @@ export const metadata: Metadata = {
     images: ["/og-image.png"],
   },
 
-  icons: {
-    icon: [
-      {
-        url: "/favicon.png",
-        sizes: "32x32",
-        type: "image/png",
-      },
-      {
-        url: "/favicon.png",
-        sizes: "16x16",
-        type: "image/png",
-      },
-    ],
-
-    apple: [
-      {
-        url: "/favicon.png",
-        sizes: "180x180",
-        type: "image/png",
-      },
-    ],
-  },
+ 
 
   metadataBase: new URL("https://axonenerji.com"),
 };
